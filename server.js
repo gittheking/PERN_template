f6e80487eccd1b67fc51a04b4d10118fdcffdb70
Bobby King <king.robertjoseph@gmail.com>
2016-11-17 01:13:52 -0500

    Add dotenv module to dependencies

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 'use strict'
+require('dotenv').config();
 const express = require('express');
 const logger  = require('morgan');
 const path    = require('path');
-
 const app     = express();
 const PORT    = process.argv[2] || process.env.port || 3000;
 
-app.use( logger('dev') );
+app.use(logger('dev'));
 
-app.use( express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.listen(PORT , () => console.log('server here! listening on', PORT ) );
+app.listen(PORT, () => console.log('server here! listening on', PORT));
